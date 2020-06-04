43d3cd08e02761bd563c4a6e2dc13c34d7a72465
Nina Primeraki <ninaprimeraki@macbook-pro.home>
2020-06-04 12:05:33 +0100

    "added files"

---
+++ app.js
@@ -0,0 +1,38 @@
+const game = () => {
+  let pScore = 0;
+  let cScore = 0;
+
+  // start the game
+  const startGame = () => {
+    const playBtn = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playBtn.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+      match.classList.add("fadeIn");
+    });
+  };
+
+  // play match
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector("player-hand");
+    const computer = document.querySelector("computer-hand");
+    //computer options
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach((option) => {
+      option.addEventListener("click", function () {
+        // computer choice
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+      });
+    });
+  };
+
+  startGame();
+  playMatch();
+};
+
+game();
